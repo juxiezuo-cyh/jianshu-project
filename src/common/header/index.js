8b8a8c851fe560bbb5e5b2fab1a54b931472c82c
juxiezuo-cyh <422738768@qq.com>
2020-12-20 23:37:46 +0800

    7-12 ajax 请求数据,没看完

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,44 +1,88 @@
-import React from 'react';
+import React, { Component }from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
-import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button } from './style';
-import  {actionCreators} from './store/index.js';
+import { 
+  HeaderWrapper, 
+  Logo, 
+  Nav, 
+  NavItem, 
+  NavSearch, 
+  Addition, 
+  Button, 
+  SearchWrapper, 
+  SearchInfo, 
+  SearchInfoTitle,
+  SearchInfoSwitch,
+  SearchInfoItem,
+  SearchInfoList } from './style';
+import { actionCreators } from './store/index.js';
 
-const Header = (props) => {
-  const { focused,addBlur,addFocus } = props;
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.getListArea = this.getListArea.bind(this);
+  }
 
-  return (
-    <HeaderWrapper>
-      <Logo />
-      <Nav>
-        <NavItem className="left active">首页</NavItem>
-        <NavItem className="left">下载App</NavItem>
-        <NavItem className="right">登录</NavItem>
-        <NavItem className="right">Aa</NavItem>
-        
-        <CSSTransition
-          timeout={200}
-          in={focused}
-          >
-          <NavSearch 
-            className={focused ? 'focused' : ''} 
-            onBlur={addBlur} 
-            onFocus={addFocus}></NavSearch>
-        </CSSTransition>
-      </Nav>
-      <Addition>
-        <Button className="writting">写文章</Button>
-        <Button className="reg">注册</Button>
-      </Addition>
-      
-    </HeaderWrapper>
-  )
+  getListArea(show) {
+    if (show) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一换 </SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>简书</SearchInfoItem>
+            <SearchInfoItem>生活</SearchInfoItem>
+            <SearchInfoItem>学习</SearchInfoItem>
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    const { focused, addBlur, addFocus } = this.props;
+    return (
+      <HeaderWrapper>
+        <Logo />
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">Aa</NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              timeout={200}
+              in={focused}
+              // className="slide"
+            >
+              <NavSearch
+                className={focused ? 'focused' : ''}
+                onBlur={addBlur}
+                onFocus={addFocus}></NavSearch>
+            </CSSTransition>
+            { this.getListArea(focused) }
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className="writting">写文章</Button>
+          <Button className="reg">注册</Button>
+        </Addition>
+  
+      </HeaderWrapper>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
   // 这个函数的作用是把store里面的数据，映射到props里面
   return {
-    focused: state.header.focused,
+    focused: state.getIn(['header', 'focused'])
+    // focused: state.get('header').get('focused'),
   }
 }
 const mapDispatchToProps = (dispatch) => {
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,20 +1,19 @@
 import * as actionTypes from './actionTypes.js';
+import { fromJS } from 'immutable'; //引入不可改变state的库；
 
-const defaultState = {
+const defaultState = fromJS({
   focused: false,
-};
+  list: [],
+});
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = defaultState, action) => {
   switch (action.type) {
     case actionTypes.SEARCH_FOCUS:
-      return {
-        focused: action.value,
-      }
+      // immutable的原理是，结合之前的immutable的值，和新的设置的值，生成一个新的对象。不会修改原来的值
+      return state.set('focused', action.value);
     case actionTypes.SEARCH_BLUR:
-      return {
-        focused: action.value,
-      }
+      return state.set('focused', action.value);
     default:
       return state;
   }
